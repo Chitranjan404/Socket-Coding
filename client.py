2315ee7384d4e39cd5bc0a5cb6ea64e5d3b96932
Chitranjan Singh <74783185+Chitranjan404@users.noreply.github.com>
2021-03-07 15:22:41 +0000

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import socket
 cli=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cli.connect(("192.168.1.11",1236))
+cli.connect(("****SERVER'S IP ADDRESS****",****PORT THROUGH WHICH SERVER AND CLIENT WILL COMMUNICATE****))
 cli.send("Hi this is my first socket codeeeeeeeeeee!")
 a=cli.recv(1024)
 print a
